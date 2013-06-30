22335ea2b10eacf59fda4b266b54ce5703944097
Gaurav Agarwal <gauravag@thoughtworks.com>
2013-06-30 20:35:21 +0530

    #8: Regularizing the Cost and gradient. P.S. COMPLETED THE COURSE!

--- Ex8/mlclass-ex8/cofiCostFunc.m
+++ Ex8/mlclass-ex8/cofiCostFunc.m
@@ -40,20 +40,20 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-J = sum((((R .* ((X * Theta') - Y))) .^ 2)(:)) / 2;
+J = (sum((((R .* ((X * Theta') - Y))) .^ 2)(:)) + (lambda * (sum(Theta(:) .^ 2) + sum(X(:) .^ 2)))) / 2;
 
 for i = 1:num_movies
 	idx = find(R(i, :) == 1);
 	Theta_temp = Theta(idx, :);
 	Y_temp = Y(i, idx);
-	X_grad(i, :) = ((X(i, :) * Theta_temp') - Y_temp) * Theta_temp;
+	X_grad(i, :) = ((X(i, :) * Theta_temp') - Y_temp) * Theta_temp + (lambda * X(i, :));
 endfor;
 
 for j = 1:num_users
 	idx = find(R(:, j) == 1);
 	X_temp = X(idx, :);
 	Y_temp = Y(idx, j);
-	Theta_grad(j, :) = ((Theta(j, :) * X_temp') - Y_temp') * X_temp;
+	Theta_grad(j, :) = ((Theta(j, :) * X_temp') - Y_temp') * X_temp + (lambda * Theta(j, :));
 endfor;
 
 % =============================================================
